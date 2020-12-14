02bda86589ea5f78fa97f2da3aa5cc29c7d99e14
Philipp Rothenhäusler <phirot@kth.se>
2020-12-14 11:03:38 +0100

    Update example script for local stream usage

--- scripts/stream_local.py
+++ scripts/stream_local.py
@@ -10,8 +10,24 @@ __status__ = "Production"
 __license__ = "BSD"
 __version__ = "1.0"
 
-from mopy import local_stream as stream
+
+import time
+
+
+from mopy import base_stream as stream
+
 
 if __name__ == "__main__":
+    # Create external vision instance
     vision_client = stream.ExternalVisionLocal()
-    vision_client.run()
+    ## Start blocking stream in main thread
+    # vision_client.run()
+    ## Start multiprocessed stream
+    vision_client.run_multiprocessed()
+
+    while vision_client.is_ok:
+        print('Continue activity')
+        rb = vision_client.bodies['SampleBodyName']
+        print('Received rigid body:', rb)
+        time.sleep(1)
+
