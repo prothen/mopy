96db9733ac61fa57986a1bcff37c64ce01a5ff79
Philipp Rothenhäusler <phirot@kth.se>
2020-12-16 20:03:00 +0100

    Update example scripts

--- scripts/stream_local.py
+++ scripts/stream_local.py
@@ -12,22 +12,29 @@ __version__ = "1.0"
 
 
 import time
-
+import numpy
+import transforms3d
 
 from mopy import base_stream as stream
 
 
 if __name__ == "__main__":
     # Create external vision instance
-    vision_client = stream.ExternalVisionLocal()
+    vision_client = stream.ExternalVision(debug_is_enabled=True)
     ## Start blocking stream in main thread
     # vision_client.run()
     ## Start multiprocessed stream
-    vision_client.run_multiprocessed()
+    #vision_client.run_multiprocessed()
+    vision_client.run_multithreaded()
+    # Change the following name to a rigid body exposed by your QTM configuration 
+    testname = "pixi"
 
+    import time
+    time.sleep(2)
     while vision_client.is_ok:
         print('Continue activity')
-        rb = vision_client.bodies['SampleBodyName']
+        print(vision_client.bodies)
+        rb = vision_client.bodies[testname]
         print('Received rigid body:', rb)
         time.sleep(1)
 
